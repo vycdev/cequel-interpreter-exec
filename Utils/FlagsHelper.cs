56592a0279c4a5cb0ad69029d42abb1913fb5bb7
Sandu Victor <vyctor661@gmail.com>
2024-02-13 20:45:36 +0200

    feat: flags functionality in their own file

---
+++ Utils/FlagsHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpreter_exec.Utils
+{
+    internal class FlagsHelper
+    {
+        public string InputFile { get; set; } = "input.ceq";
+        public string OutputFile { get; set; } = string.Empty;
+        public bool Verbose { get; set; }
+        public bool Debug { get; set; }
+        public bool Help { get; set; }
+ 
+        public FlagsHelper(string[] args) 
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                switch (args[i])
+                {
+                    case "-i":
+                    case "--input":
+                        InputFile = args[++i];
+                        if(InputFile.Contains("\"") || InputFile.Contains("'"))
+                            throw new ArgumentException("Invalid input file name");
+                        break;
+                    case "-o":
+                    case "--output":
+                        OutputFile = args[++i];
+                        if (OutputFile.Contains("\"") || OutputFile.Contains("'"))
+                            throw new ArgumentException("Invalid output file name");
+                        break;
+                    case "-v":
+                    case "--verbose":
+                        Verbose = true;
+                        break;
+                    case "-d":
+                    case "--debug":
+                        Debug = true;
+                        break;
+                    case "-h":
+                    case "-?":
+                    case "--help":
+                        Help = true;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
