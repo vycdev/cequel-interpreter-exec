d0d0d551b0a98833d9e8991735db35ee6dd22779
Sandu Victor <vyctor661@gmail.com>
2024-03-21 21:28:13 +0200

    feat: utility print tree function for printing trees in the terminal for debugging

---
+++ Utils/PrintTree.cs
@@ -0,0 +1,24 @@
+﻿using Interpreter_lib.Parser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interpreter_exec.Utils
+{
+
+    public static class PrintTree
+    {
+        public static void Print(Node tree, string indent = "", bool last = false)
+        {
+            Console.Write(indent + "+- " + tree.GetRule().ToString() + " | (" + string.Join(",", tree.GetTokens().Select(a => "[" + a.Type.ToString() + "," + a.Value + "]")) + ")");
+            indent += last ? "   " : "|  ";
+
+            for (int i = 0; i < tree.GetNodes().Count; i++)
+            {
+                Print(tree.GetNodes()[i], indent, i == tree.GetNodes().Count - 1);
+            }
+        }
+    }
+}
