6cbbe1fd868f57481dc42c3cade62964855c065c
Sandu Victor <vyctor661@gmail.com>
2024-06-10 17:15:25 +0300

    chore: using .net 8 language features

--- Program.cs
+++ Program.cs
@@ -5,115 +5,114 @@ using Interpreter_lib.Parser;
 using Interpreter_lib.Tokenizer;
 using Interpreter_lib.Utils;
 
-namespace Interpreter_exec
+namespace Interpreter_exec;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        FlagsHelper flags = new(args);
+        DateTime? executionStart = null;
+        DateTime? executionEnd = null;
+        TimeSpan? totalExecutionTime = null; 
+
+        if (flags.Help)
         {
-            FlagsHelper flags = new(args);
-            DateTime? executionStart = null;
-            DateTime? executionEnd = null;
-            TimeSpan? totalExecutionTime = null; 
+            StreamReader reader = new("help.txt");
+            string help = reader.ReadToEnd();
+            reader.Close();
+            Console.Write(help);
 
-            if (flags.Help)
+            return;
+        }
+
+        if (flags.Verbose)
+        {
+            Console.WriteLine("Input file: " + flags.InputFile);
+            Console.WriteLine("Output file: " + flags.OutputFile);
+            Console.WriteLine("Verbose: " + flags.Verbose);
+            Console.WriteLine("Debug: " + flags.Debug);
+        }
+
+        // Check if input file exists
+        if (!File.Exists(flags.InputFile))
+        {
+            Console.WriteLine($"Input file \"{flags.InputFile}\" not found.");
+            Environment.Exit(1);
+        }
+
+        if (!string.IsNullOrEmpty(flags.OutputFile) && !File.Exists(flags.OutputFile))
+            File.Create(flags.OutputFile);
+
+        // Read input file
+        StreamReader inputReader = new(flags.InputFile);
+        string code = inputReader.ReadToEnd();
+        inputReader.Close();
+
+        // Tokenize 
+        executionStart = DateTime.Now;
+
+        Tokenizer tokenizer = new(code, flags.Language);
+        
+        executionEnd = DateTime.Now;
+
+        if (flags.Debug)
+        {
+            Printer.PrintTokens(tokenizer.Tokens);
+
+        }
+        
+        if (flags.Verbose)
+        {
+            Console.WriteLine("\u001b[39m\u001b[94mTokenization time: \u001b[39m" + (executionEnd - executionStart).Value.ToString("c"));
+            totalExecutionTime = (executionEnd - executionStart).Value;
+        }
+
+        try
+        {
+            // Parse
+            executionStart = DateTime.Now;
+            
+            Parser parser = new(tokenizer.Tokens);
+            parser.Parse();
+
+            executionEnd = DateTime.Now;
+
+            // Print tree
+            if (flags.Debug)
             {
-                StreamReader reader = new("help.txt");
-                string help = reader.ReadToEnd();
-                reader.Close();
-                Console.Write(help);
-
-                return;
+                Console.WriteLine("\nSyntax tree:");
+                Printer.PrintTree(parser.GetTree());
+                Console.WriteLine("\n");
             }
 
             if (flags.Verbose)
             {
-                Console.WriteLine("Input file: " + flags.InputFile);
-                Console.WriteLine("Output file: " + flags.OutputFile);
-                Console.WriteLine("Verbose: " + flags.Verbose);
-                Console.WriteLine("Debug: " + flags.Debug);
+                Console.WriteLine("\u001b[39m\u001b[94mParsing time: \u001b[39m" + (executionEnd - executionStart).Value.ToString("c"));
+                totalExecutionTime += (executionEnd - executionStart).Value;
             }
 
-            // Check if input file exists
-            if (!File.Exists(flags.InputFile))
-            {
-                Console.WriteLine($"Input file \"{flags.InputFile}\" not found.");
-                Environment.Exit(1);
-            }
-
-            if (!string.IsNullOrEmpty(flags.OutputFile) && !File.Exists(flags.OutputFile))
-                File.Create(flags.OutputFile);
-
-            // Read input file
-            StreamReader inputReader = new(flags.InputFile);
-            string code = inputReader.ReadToEnd();
-            inputReader.Close();
-
-            // Tokenize 
+            // Execute
             executionStart = DateTime.Now;
 
-            Tokenizer tokenizer = new(code, flags.Language);
+            Evaluator evaluator = new();
+            evaluator.Evaluate(parser.GetTree());
             
             executionEnd = DateTime.Now;
 
-            if (flags.Debug)
-            {
-                Printer.PrintTokens(tokenizer.Tokens);
-
-            }
-            
             if (flags.Verbose)
             {
-                Console.WriteLine("\u001b[39m\u001b[94mTokenization time: \u001b[39m" + (executionEnd - executionStart).Value.ToString("c"));
-                totalExecutionTime = (executionEnd - executionStart).Value;
+                Console.WriteLine("\n\n\u001b[39m\u001b[94mExecution time: \u001b[39m" + (executionEnd - executionStart).Value.ToString("c"));
+                totalExecutionTime += (executionEnd - executionStart).Value;
+                Console.WriteLine("\u001b[39m\u001b[94mTotal execution time: \u001b[39m" + totalExecutionTime.Value.ToString("c"));
             }
 
-            try
-            {
-                // Parse
-                executionStart = DateTime.Now;
-                
-                Parser parser = new(tokenizer.Tokens);
-                parser.Parse();
-
-                executionEnd = DateTime.Now;
-
-                // Print tree
-                if (flags.Debug)
-                {
-                    Console.WriteLine("\nSyntax tree:");
-                    Printer.PrintTree(parser.GetTree());
-                    Console.WriteLine("\n");
-                }
-
-                if (flags.Verbose)
-                {
-                    Console.WriteLine("\u001b[39m\u001b[94mParsing time: \u001b[39m" + (executionEnd - executionStart).Value.ToString("c"));
-                    totalExecutionTime += (executionEnd - executionStart).Value;
-                }
-
-                // Execute
-                executionStart = DateTime.Now;
-
-                Evaluator evaluator = new();
-                evaluator.Evaluate(parser.GetTree());
-                
-                executionEnd = DateTime.Now;
-
-                if (flags.Verbose)
-                {
-                    Console.WriteLine("\n\n\u001b[39m\u001b[94mExecution time: \u001b[39m" + (executionEnd - executionStart).Value.ToString("c"));
-                    totalExecutionTime += (executionEnd - executionStart).Value;
-                    Console.WriteLine("\u001b[39m\u001b[94mTotal execution time: \u001b[39m" + totalExecutionTime.Value.ToString("c"));
-                }
-
-
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("\n" + ex.Message);
-            }
 
         }
+        catch (Exception ex)
+        {
+            Console.WriteLine("\n" + ex.Message);
+        }
+
     }
 }
--- Utils/Printer.cs
+++ Utils/Printer.cs
@@ -7,58 +7,57 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace interpreter_exec.Utils
+namespace interpreter_exec.Utils;
+
+
+public static class Printer
 {
-
-    public static class Printer
+    public static void PrintTokens(List<Token> tokens)
     {
-        public static void PrintTokens(List<Token> tokens)
+        Console.WriteLine(String.Format("|{0,-30}|{1,-30}|{2,-10}|", "Type", "Value", "Line"));
+        Console.WriteLine(String.Format("|{0,-30}|{0,-30}|{1,-10}|", "------------------------------", "----------"));
+        foreach (Token token in tokens)
         {
-            Console.WriteLine(String.Format("|{0,-30}|{1,-30}|{2,-10}|", "Type", "Value", "Line"));
-            Console.WriteLine(String.Format("|{0,-30}|{0,-30}|{1,-10}|", "------------------------------", "----------"));
-            foreach (Token token in tokens)
+            if (token.Value.Length > 30)
             {
-                if (token.Value.Length > 30)
-                {
-                    var lines = token.Value.ReplaceLineEndings("\\n").SplitInParts(30);
-                    Console.WriteLine(String.Format("|{0,-30}|{1,-30}|{2,-10}|", token.Type, lines.First(), token.Line));
-                    foreach (string line in lines.Skip(1))
-                        Console.WriteLine(String.Format("|{0,-30}|{1,-30}|{2,-10}|", "", line, ""));
-                }
-                else
-                    Console.WriteLine(String.Format("|{0,-30}|{1,-30}|{2,-10}|", token.Type, token.Value, token.Line));
-            }
-            Console.WriteLine("\u001b[39m\u001b[94mNumber of tokens: \u001b[39m" + tokens.Count());
-        }
-
-        public static void PrintTree(ISyntaxNode node, string indent = "", bool last = false)
-        {
-            var regexColon = new Regex(Regex.Escape(":"));
-            var regexSeparator = new Regex(Regex.Escape("|"));
-            string name = "\u001b[39m";
-
-            if(node.GetType() == typeof(Node))
-            {
-                name += "\x1b[94m" + node.Print();
-                name = regexSeparator.Replace(name, "\u001b[39m|\u001b[92m", 1);
-                name = regexColon.Replace(name, ":\u001b[39m");
+                var lines = token.Value.ReplaceLineEndings("\\n").SplitInParts(30);
+                Console.WriteLine(String.Format("|{0,-30}|{1,-30}|{2,-10}|", token.Type, lines.First(), token.Line));
+                foreach (string line in lines.Skip(1))
+                    Console.WriteLine(String.Format("|{0,-30}|{1,-30}|{2,-10}|", "", line, ""));
             }
             else
-            {
-                name += "\x1b[93m" + regexColon.Replace(node.Print(), ":\u001b[39m");
-                name = regexSeparator.Replace(name, "\u001b[39m|\u001b[92m", 1);
-            }
-
-            name += "\u001b[39m";
-
-            Console.Write("\n" + indent + "+- " + name);
-            indent += last ? "   " : "|  ";
-
-            if(node.GetType() == typeof(Node))
-                for (int i = 0; i < ((Node)node).GetSyntaxNodes().Count; i++)
-                {
-                    PrintTree(((Node)node).GetSyntaxNodes()[i], indent, i == ((Node)node).GetSyntaxNodes().Count - 1);
-                }
+                Console.WriteLine(String.Format("|{0,-30}|{1,-30}|{2,-10}|", token.Type, token.Value, token.Line));
         }
+        Console.WriteLine("\u001b[39m\u001b[94mNumber of tokens: \u001b[39m" + tokens.Count());
+    }
+
+    public static void PrintTree(ISyntaxNode node, string indent = "", bool last = false)
+    {
+        var regexColon = new Regex(Regex.Escape(":"));
+        var regexSeparator = new Regex(Regex.Escape("|"));
+        string name = "\u001b[39m";
+
+        if(node.GetType() == typeof(Node))
+        {
+            name += "\x1b[94m" + node.Print();
+            name = regexSeparator.Replace(name, "\u001b[39m|\u001b[92m", 1);
+            name = regexColon.Replace(name, ":\u001b[39m");
+        }
+        else
+        {
+            name += "\x1b[93m" + regexColon.Replace(node.Print(), ":\u001b[39m");
+            name = regexSeparator.Replace(name, "\u001b[39m|\u001b[92m", 1);
+        }
+
+        name += "\u001b[39m";
+
+        Console.Write("\n" + indent + "+- " + name);
+        indent += last ? "   " : "|  ";
+
+        if(node.GetType() == typeof(Node))
+            for (int i = 0; i < ((Node)node).GetSyntaxNodes().Count; i++)
+            {
+                PrintTree(((Node)node).GetSyntaxNodes()[i], indent, i == ((Node)node).GetSyntaxNodes().Count - 1);
+            }
     }
 }
